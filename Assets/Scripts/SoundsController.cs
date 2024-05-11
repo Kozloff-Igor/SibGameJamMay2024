77df4c2efba39b21525c4f2e71054a38fb21e453
Igor <53177927+Kozloff-Igor@users.noreply.github.com>
2024-05-11 21:04:48 +0700

    Sounds controller update

--- Assets/Scripts/SoundsController.cs
+++ Assets/Scripts/SoundsController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum SoundClipType { Whales, CrystalAppear, CrystalMelody, CrystalFinal, FootSteps, HookThrow, HookGrab, HookTake, Cogs, Pipes, Water, MinigameDone }
@@ -8,10 +9,17 @@ public class SoundsController : MonoBehaviour
 {
     public static SoundsController Instance;
     public AudioSource loopingAmbient;
+
     public AudioSource planetMusic;
     public AudioSource loopingHook;
     public AudioSource playerSource;
     public AudioSource sourceInSpace;
+
+    public Transform[] musicSourcesPlaces;
+    Transform closestMusicTransform;
+
+    public AudioClip[] musicClips;
+    public Transform player;
 
     [System.Serializable]
     public struct Clips
@@ -26,9 +34,10 @@ public class SoundsController : MonoBehaviour
     [Header("_____")]
     public Vector2 whalesCooldown;
 
+    public bool gameIsNotFinished = true;
 
     void Awake()
-    {        
+    {
         Instance = this;
         clipsIndexes = new int[allClips.Length];
         for (int q = 0; q < clipsIndexes.Length; q++)
@@ -43,11 +52,39 @@ public class SoundsController : MonoBehaviour
         }
 
         StartCoroutine(WhailingWhales());
+        StartCoroutine(FindClosestMusic());
     }
 
     void Update()
     {
+        float dist = Vector3.Distance(closestMusicTransform.position, player.transform.position);
+        if (dist < 30f) 
+        {
+//            closestMusic
 
+        }
+
+
+    }
+
+    IEnumerator FindClosestMusic()
+    {
+        while (gameIsNotFinished)
+        {
+            float closestDist = Vector3.Distance(player.position, musicSourcesPlaces[0].transform.position);
+            closestMusicTransform = musicSourcesPlaces[0];
+            for (int q = 1; q < musicSourcesPlaces.Length; q++)
+            {
+                float dist = Vector3.Distance(player.position, musicSourcesPlaces[q].transform.position);
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closestMusicTransform = musicSourcesPlaces[q];
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     public void PlaySound(SoundClipType soundClipType)
@@ -72,7 +109,7 @@ public class SoundsController : MonoBehaviour
 
     IEnumerator WhailingWhales()
     {
-        while(true)
+        while (true)
         {
             yield return new WaitForSeconds(Random.Range(whalesCooldown.x, whalesCooldown.y));
             int id = clipsIndexes[(int)SoundClipType.Whales];
